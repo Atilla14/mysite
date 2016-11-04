32bb318023f23641bade352c097678d05c1cdea3
atilla_14 <sathyanarayanravi@gmail.com>
2016-11-04 13:58:09 +0530

    Update launch date

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var date = countdown( new Date(1477852200000) )
+  var date = countdown( new Date(1478716200000) )
   var dateString = date.toString()
   var hours1 = document.getElementById('hours-1')
   var hours2 = document.getElementById('hours-2')
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', function() {
   var seconds2 = document.getElementById('seconds-2')
 
   setInterval(function() {
-    date = countdown( new Date(1477852200000) )
+    date = countdown( new Date(1478716200000) )
     dateString = date.toString()
     hours1.innerText = toLocaleString(date.hours, 2)[0]
     hours2.innerText = toLocaleString(date.hours, 2)[1]
